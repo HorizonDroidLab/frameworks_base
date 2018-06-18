4e56f6476dbfabc76a36eb71f2f644fb98668341
Jason Edson <jaysonedson@gmail.com>
2018-06-17 17:44:52 -0700

    SystemUI: QS: Add On-The-Go Tile
    @neobuddy89: Updated for A12, A13, A14
    
    Squashed:
    
        From: ShevT <evgeny.shishkov@gmail.com>
        Date: Wed, 23 Feb 2022 22:37:52 +0300
        Subject: OnTheGoTile: Fixed refresh state when turning off tile
    
        Test:
        1) Click on the tile. The On-The-Go mode is activated.
           The tile becomes active.
        2) Click on the tile again. On-The-Go mode turns off.
           The tile remains active until we close quick settings and reopen.
    
        Let's fix this.
    
    Change-Id: Ic345f83723a48e3a7d610926fb9b47fab7ef878f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ryzenforce990 <ryzenforce990@gmail.com>

--- packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
+++ packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
@@ -22,6 +22,7 @@ import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.HeadsUpTile
+import com.android.systemui.qs.tiles.OnTheGoTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VpnTile
@@ -65,6 +66,12 @@ interface CustomModule {
     @IntoMap
     @StringKey(HeadsUpTile.TILE_SPEC)
     fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
+    /** Inject OnTheGoTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(OnTheGoTile.TILE_SPEC)
+    fun bindOnTheGoTile(onTheGoTile: OnTheGoTile): QSTileImpl<*>
 
     /** Inject SyncTile into tileMap in QSModule */
     @Binds
