d39e3dbd01ff211bf75f40eec7e4a226a56467c3
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-07-06 01:35:10 +0000

    SystemUI: Make colored Statusbar Icons optional [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    SystemUI: Refactor colored statusbar icons
    
    * Remove painful systemui checks everywhere in StatusBariconView
    * Apply NO_COLOR when colored icons are enabled.
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: spkal01 <kalligeross@gmail.com>
    Signed-off-by: ryzenforce990 <ryzenforce990@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6139,6 +6139,12 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
 
         /**
+         * Whether StatusBar icons should use app icon
+         * @hide
+         */
+        public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
