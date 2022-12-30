e5ae162c6febdeeb84962cdf03c51d1bb0fe9073
minaripenguin <minaripenguin@users.noreply.github.com>
2022-12-30 22:54:17 +0800

    SystemUI: Import breathing UDFPS animation from nothingOS
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
     Conflicts:
    	packages/SystemUI/res/layout/udfps_keyguard_view_internal.xml
    	packages/SystemUI/src/com/android/systemui/biometrics/UdfpsKeyguardViewLegacy.java

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsKeyguardViewLegacy.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsKeyguardViewLegacy.java
@@ -24,7 +24,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
-import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
@@ -188,11 +188,7 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
             return;
         }
 
-        mTextColorPrimary = Utils.getColorAttrDefaultColor(mContext,
-                com.android.internal.R.attr.materialColorOnSurface);
-        final int backgroundColor = Utils.getColorAttrDefaultColor(getContext(),
-                com.android.internal.R.attr.materialColorSurfaceContainerHigh);
-        mBgProtection.setImageTintList(ColorStateList.valueOf(backgroundColor));
+        mTextColorPrimary = Color.parseColor("#FFFFFF");
         mLockScreenFp.invalidate(); // updated with a valueCallback
     }
 
@@ -205,11 +201,12 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
             return;
         }
 
+
         final int defaultPaddingPx =
                 getResources().getDimensionPixelSize(R.dimen.lock_icon_padding);
         final int padding = (int) (defaultPaddingPx * mScaleFactor);
-        mLockScreenFp.setPadding(padding, padding, padding, padding);
-        mAodFp.setPadding(padding, padding, padding, padding);
+        mLockScreenFp.setPadding(0, 0, 0, 0);
+        mAodFp.setPadding(0, 0, 0, 0);
     }
 
     /**
